f1f75de1bd6801d63cf1a3cf255aefefb3c2166e
ivanturbin <ivanturbin2004@gmail.com>
2025-04-08 15:02:10 +0300

    -

--- WebRandom/game_builds/urls.py
+++ WebRandom/game_builds/urls.py
@@ -6,8 +6,8 @@ app_name = 'game_builds'
 urlpatterns = [
     path('', views.game_list, name='game_list'),
     path('game/<slug:slug>/', views.game_detail, name='game_detail'),
-    path('game/<slug:slug>/generate/', views.generate_random_build, name='generate_build'),
+    path('game/<slug:slug>/generate/', views.generate_random_build, name='generate_random_build'),
     path('register-admin/', views.register_admin, name='register_admin'),
     path('activate-admin/<uuid:code>/', views.activate_admin_invite, name='activate_admin'),
     path('register/', views.register, name='register'),
-] 
+]
