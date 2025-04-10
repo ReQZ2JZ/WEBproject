ef8c8ca8a6a7d80ace1b6ca9c7079927c772859b
Magmarovich <alexpaletskiy@gmail.com>
2025-04-10 19:12:06 +0300

    Add test DBD random

--- WebRandom/game_builds/games/dead_by_daylight.py
+++ WebRandom/game_builds/games/dead_by_daylight.py
@@ -1,10 +1,27 @@
 import random
 
 def generate_dead_by_daylight_build():
-    perks = ["Borrowed Time", "Dead Hard", "Iron Will", "Sprint Burst"]
-    items = ["Medkit", "Flashlight", "Toolbox", "Map"]
-
-    return {
-        "perks": random.sample(perks, 4),
-        "item": random.choice(items)
+    roles = {
+        "survivor": ["Дуайт", "Мегги", "Джей", "Клодета", "Нея"],
+        "killer": ["Трапер", "Призрак", "Хилли Билли", "Медсестра", "Анна"]
     }
+
+    # Случайно выбираем роль
+    role = random.choice(list(roles.keys()))
+    character = random.choice(roles[role])
+
+    # Добавляем URL картинки персонажа
+    character_image = f"/static/images/characters/{role}/{character}.png"
+
+    perks = ["Borrowed Time", "Dead Hard", "Iron Will", "Sprint Burst"]
+    items = ["Medkit", "Flashlight", "Toolbox", "Map"] if role == "survivor" else []
+
+    build = {
+        "role": role,
+        "character": character,
+        "character_image_png": character_image,
+        "perks": random.sample(perks, 4),
+        "item": random.choice(items) if items else None
+    }
+
+    return build
