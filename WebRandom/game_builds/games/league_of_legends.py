9abf778d19c2ecef621707e7cc5aa7b861c950d3
Howgad <ggwprip@mail.ru>
2025-04-11 15:36:03 +0300

    Add some interface for random

--- WebRandom/game_builds/games/league_of_legends.py
+++ WebRandom/game_builds/games/league_of_legends.py
@@ -4,25 +4,22 @@ def generate_league_of_legends_build():
     def format_image_name(name):
         return name.replace(" ", "").replace("'", "") + ".png"
 
-    champions = [
-        "Ahri", "Akali", "Akshan", "Alistar", "Amumu", "Anivia", "Annie", "Aphelios", "Ashe", "Aurelion Sol", "Azir",
-        "Bard", "Bel'Veth", "Blitzcrank", "Brand", "Braum", "Briar", "Caitlyn", "Camille", "Cassiopeia", "Cho'Gath",
-        "Corki", "Darius", "Diana", "Dr. Mundo", "Draven", "Ekko", "Elise", "Evelynn", "Ezreal",
-"Fiddlesticks", "Fiora", "Fizz", "Galio", "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Gwen",
-"Hecarim", "Heimerdinger", "Hwei", "Illaoi", "Irelia", "Ivern", "Janna", "Jarvan IV", "Jax", "Jayce",
-"Jhin", "Jinx", "K'Sante", "Kai'Sa", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle",
-"Kayn", "Kennen", "Kha'Zix", "Kindred", "Kled", "Kog'Maw", "LeBlanc", "Lee Sin", "Leona", "Lillia",
-"Lissandra", "Lucian", "Lulu", "Lux", "Malphite", "Malzahar", "Maokai", "Master Yi", "Milio", "Miss Fortune",
-"Mordekaiser", "Morgana", "Naafiri", "Nami", "Nasus", "Nautilus", "Neeko", "Nidalee", "Nilah", "Nocturne",
-"Nunu & Willump", "Olaf", "Orianna", "Ornn", "Pantheon", "Poppy", "Pyke", "Qiyana", "Quinn", "Rakan",
-"Rammus", "Rek'Sai", "Rell", "Renata Glasc", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Samira",
-"Sejuani", "Senna", "Seraphine", "Sett", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir",
-"Skarner", "Smolder", "Sona", "Soraka", "Swain", "Sylas", "Syndra", "Tahm Kench", "Taliyah", "Talon",
-"Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "Twisted Fate", "Twitch", "Udyr", "Urgot",
-"Varus", "Vayne", "Veigar", "Vel'Koz", "Vex", "Vi", "Viego", "Viktor", "Vladimir", "Volibear",
-"Warwick", "Wukong", "Xayah", "Xerath", "Xin Zhao", "Yasuo", "Yone", "Yorick", "Yuumi", "Zac",
-"Zed", "Zeri", "Ziggs", "Zilean", "Zoe", "Zyra"
-
+    champions = [ "Ahri", "Akali", "Akshan", "Alistar", "Amumu", "Anivia", "Annie", "Aphelios", "Ashe", "Aurelion Sol",
+        "Azir", "Bard", "Bel'Veth", "Blitzcrank", "Brand", "Braum", "Briar", "Caitlyn", "Camille", "Cassiopeia", 
+        "Cho'Gath", "Corki", "Darius", "Diana", "Draven", "Ekko", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", 
+        "Fiora", "Fizz", "Galio", "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Gwen", "Hecarim", "Heimerdinger", 
+        "Hwei", "Illaoi", "Irelia", "Ivern", "Janna", "Jarvan IV", "Jax", "Jayce", "Jhin", "Jinx", "K'Sante", "Kai'Sa", 
+        "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kayn", "Kennen", "Kha'Zix", "Kindred", "Kled", 
+        "Kog'Maw", "LeBlanc", "Lee Sin", "Leona", "Lillia", "Lissandra", "Lucian", "Lulu", "Lux", "Malphite", "Malzahar", 
+        "Maokai", "Master Yi", "Milio", "Miss Fortune", "Mordekaiser", "Morgana", "Naafiri", "Nami", "Nasus", "Nautilus", 
+        "Neeko", "Nidalee", "Nilah", "Nocturne", "Nunu & Willump", "Olaf", "Orianna", "Ornn", "Pantheon", "Poppy", 
+        "Pyke", "Qiyana", "Quinn", "Rakan", "Rammus", "Rek'Sai", "Rell", "Renata Glasc", "Renekton", "Rengar", "Riven", 
+        "Rumble", "Ryze", "Samira", "Sejuani", "Senna", "Seraphine", "Sett", "Shaco", "Shen", "Shyvana", "Singed", 
+        "Sion", "Sivir", "Skarner", "Smolder", "Sona", "Soraka", "Swain", "Sylas", "Syndra", "Tahm Kench", "Taliyah", 
+        "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "Twisted Fate", "Twitch", "Udyr", 
+        "Urgot", "Varus", "Vayne", "Veigar", "Vel'Koz", "Vex", "Vi", "Viego", "Viktor", "Vladimir", "Volibear", 
+        "Warwick", "Wukong", "Xayah", "Xerath", "Xin Zhao", "Yasuo", "Yone", "Yorick", "Yuumi", "Zac", "Zed", "Zeri", 
+        "Ziggs", "Zilean", "Zoe", "Zyra"
     ]
 
     mythic_items = [
@@ -52,16 +49,18 @@ def generate_league_of_legends_build():
         "Silvermere Dawn", "Chempunk Chainsword", "Knight's Vow", "Zeke's Convergence"
     ]
 
-    selected = {
-        "mythic": random.choice(mythic_items),
-        "legendary": random.choice(legendary_items),
-        "boots": random.choice(boots),
-        "unique": random.choice(unique_items)
-    }
+    # 1 предмет из каждой категории
+    mythic = random.choice(mythic_items)
+    legendary = random.choice([item for item in legendary_items if item != mythic])
+    boots_item = random.choice([item for item in boots if item not in [mythic, legendary]])
+    unique = random.choice([item for item in unique_items if item not in [mythic, legendary, boots_item]])
 
-    pool = list(set(mythic_items + legendary_items + boots + unique_items) - set(selected.values()))
-    extra_items = random.sample(pool, 2)
-    final_items = list(selected.values()) + extra_items
+    used = {mythic, legendary, boots_item, unique}
+    remaining_pool = [item for item in (mythic_items + legendary_items + boots + unique_items) if item not in used]
+    extra_items = random.sample(remaining_pool, 2)
+
+    items = [mythic, legendary, boots_item, unique] + extra_items
+    categories = ['Mythic', 'Legendary', 'Boots', 'Unique', 'Extra', 'Extra']
 
     champion = random.choice(champions)
 
@@ -73,8 +72,9 @@ def generate_league_of_legends_build():
         "items": [
             {
                 "name": item,
-                "image": f"/media/item_images/{format_image_name(item)}"
+                "image": f"/media/item_images/{format_image_name(item)}",
+                "category": categories[i]
             }
-            for item in final_items
+            for i, item in enumerate(items)
         ]
     }
