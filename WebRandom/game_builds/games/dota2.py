d5a0e20348bf16fe0e6b4651ec4f2eb0359771ea
ivanturbin <ivanturbin2004@gmail.com>
2025-04-12 07:59:33 +0300

    a more beautiful visual

--- WebRandom/game_builds/games/dota2.py
+++ WebRandom/game_builds/games/dota2.py
@@ -1,57 +1,75 @@
 import random
+from django.shortcuts import render
 
 def format_item_filename(item):
-    # Приводит имя к нижнему регистру, заменяет пробелы на подчёркивания и удаляет апострофы
     return item.lower().replace(' ', '_').replace("'", "")
 
-def generate_dota2_build():
-    heroes = [
-        "Abaddon", "Alchemist", "Axe", "Beastmaster", "Brewmaster", "Bristleback", "Broodmother", 
-        "Centaur Warrunner", "Chaos Knight", "Clockwerk", "Dawnbreaker", "Doom", "Dragon Knight", 
-        "Earth Spirit", "Earthshaker", "Elder Titan", "Huskar", "Io", "Kunkka", "Legion Commander", 
-        "Lifestealer", "Lycan", "Magnus", "Marci", "Mars", "Night Stalker", "Omniknight", "Phoenix", 
-        "Primal Beast", "Pudge", "Sand King", "Slardar", "Snapfire", "Spirit Breaker", "Sven", 
-        "Tidehunter", "Timbersaw", "Tiny", "Treant Protector", "Tusk", "Underlord", "Undying", "Wraith King",
-        "Anti-Mage", "Arc Warden", "Bloodseeker", "Bounty Hunter", "Clinkz", "Crystal Maiden", 
-        "Drow Ranger", "Ember Spirit", "Faceless Void", "Gyrocopter", "Hoodwink", "Juggernaut", 
-        "Lone Druid", "Luna", "Medusa", "Meepo", "Mirana", "Monkey King", "Morphling", "Naga Siren", 
-        "Nyx Assassin", "Pangolier", "Phantom Assassin", "Phantom Lancer", "Razor", "Riki", 
-        "Shadow Fiend", "Slark", "Sniper", "Spectre", "Templar Assassin", "Terrorblade", 
-        "Troll Warlord", "Ursa", "Vengeful Spirit", "Venomancer", "Viper", "Weaver",
-        "Ancient Apparition", "Bane", "Batrider", "Crystal Maiden", "Dark Seer", "Dark Willow", 
-        "Dazzle", "Death Prophet", "Disruptor", "Enchantress", "Enigma", "Grimstroke", "Invoker", 
-        "Jakiro", "Keeper of the Light", "Leshrac", "Lich", "Lina", "Lion", "Nature's Prophet", 
-        "Necrophos", "Oracle", "Outworld Destroyer", "Puck", "Queen of Pain", "Rubick", "Shadow Demon", 
-        "Shadow Shaman", "Silencer", "Skywrath Mage", "Storm Spirit", "Techies", "Tinker", "Visage", 
-        "Void Spirit", "Warlock", "Witch Doctor", "Zeus"
+heroes_by_role = {
+    "Carry": [
+        "Anti-Mage", "Faceless Void", "Juggernaut", "Terrorblade", "Spectre",
+        "Phantom Assassin", "Riki", "Drow Ranger", "Medusa", "Morphling",
+        "Luna", "Slark", "Sniper", "Ursa", "Troll Warlord", "Naga Siren",
+        "Monkey King", "Chaos Knight", "Lifestealer", "Wraith King"
+    ],
+    "Midlaner": [
+        "Shadow Fiend", "Templar Assassin", "Invoker", "Storm Spirit", "Queen of Pain",
+        "Puck", "Tinker", "Ember Spirit", "Zeus", "Outworld Destroyer", "Huskar",
+        "Death Prophet", "Leshrac", "Lina", "Dragon Knight", "Batrider", "Arc Warden",
+        "Void Spirit"
+    ],
+    "Offlaner": [
+        "Axe", "Mars", "Timbersaw", "Centaur Warrunner", "Underlord", "Doom",
+        "Bristleback", "Slardar", "Tidehunter", "Night Stalker", "Sand King",
+        "Beastmaster", "Pangolier", "Dark Seer", "Magnus", "Broodmother",
+        "Primal Beast", "Abaddon", "Clockwerk", "Treant Protector"
+    ],
+    "Support": [
+        "Crystal Maiden", "Lich", "Witch Doctor", "Warlock", "Jakiro", "Disruptor",
+        "Bane", "Shadow Shaman", "Lion", "Dazzle", "Omniknight", "Oracle", "Io",
+        "Grimstroke", "Rubick", "Dark Willow", "Enchantress", "Chen", "Silencer",
+        "Techies", "Keeper of the Light", "Ancient Apparition", "Skywrath Mage",
+        "Shadow Demon", "Vengeful Spirit"
+    ],
+    "Universal": [
+        "Snapfire", "Marci", "Tusk", "Earthshaker", "Earth Spirit", "Phoenix",
+        "Bounty Hunter", "Pudge", "Nyx Assassin", "Meepo", "Mirana", "Clinkz",
+        "Hoodwink", "Weaver", "Spirit Breaker", "Venomancer", "Nature's Prophet",
+        "Enigma", "Visage", "Necrophos", "Alchemist", "Leshrac", "Viper", "Lion",
+        "Underlord", "Invoker"
     ]
+}
 
-    items = {
-        "Late Game": [
-            "Abyssal Blade", "Assault Cuirass", "Battle Fury", "Black King Bar", "Bloodthorn",
-            "Butterfly", "Daedalus", "Desolator", "Divine Rapier", "Eye of Skadi", "Heart of Tarrasque",
-            "Linken's Sphere", "Manta Style", "Monkey King Bar", "Nullifier", "Octarine Core",
-            "Radiance", "Refresher Orb", "Satanic", "Scythe of Vyse", "Shiva's Guard",
-            "Skull Basher", "Sphere", "Travel Boots", "Vladmir's Offering"
-        ]
-    }
+late_game_items = [
+    "Abyssal Blade", "Assault Cuirass", "Battle Fury", "Black King Bar", "Bloodthorn",
+    "Butterfly", "Daedalus", "Desolator", "Divine Rapier", "Eye of Skadi", "Heart of Tarrasque",
+    "Linken's Sphere", "Manta Style", "Monkey King Bar", "Nullifier", "Octarine Core",
+    "Radiance", "Refresher Orb", "Satanic", "Scythe of Vyse", "Shiva's Guard",
+    "Skull Basher", "Sphere", "Travel Boots", "Vladmir's Offering"
+]
 
-    # Выбираем случайного героя и формируем URL его изображения
-    hero = random.choice(heroes)
+def generate_dota2_build(role=None):
+    all_heroes = sum(heroes_by_role.values(), [])
+    selected_pool = heroes_by_role.get(role, all_heroes)
+    hero = random.choice(selected_pool)
     hero_image = f"/static/images/heroes/{format_item_filename(hero)}.png"
 
-    # Выбираем 6 late game предметов
-    late_items = [
+    items = [
         {
-            "name": item, 
+            "name": item,
             "image_png": f"/static/images/items/{format_item_filename(item)}.png"
-        } 
-        for item in random.sample(items["Late Game"], 6)
+        }
+        for item in random.sample(late_game_items, 6)
     ]
 
-    build = {
+    return {
         "hero": hero,
         "hero_image_png": hero_image,
-        "late_items": late_items,
+        "late_items": items,
+        "role": role or "Random"
     }
-    return build
+
+def game_detail(request, slug):
+    role = request.GET.get("role")
+    build = generate_dota2_build(role)
+    game = {"name": "Dota 2"}
+    return render(request, 'game_builds/random_build.html', {'game': game, 'build': build})
